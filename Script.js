8708703772bf4cebd286bcd62fa7654e9b73bf16
Deforis Nash <Deforis Nash>
2016-11-23 11:29:05 -0600

    Initial javascroopt

---
+++ Script.js
@@ -0,0 +1,31 @@
+var atticus = ["Atticus", "2405", "47000", 3];
+var jem = ["Jem", "62347", "63500", 4];
+var boo = ["Boo", "11435", "54000", 3];
+var scout = ["Scout", "6243", "74750", 5];
+var robert = ["Robert", "26835", "66000", 1];
+var mayella = ["Mayella", "89068", "35000", 2];
+var employeeName = "Boo";
+var employees = [atticus, jem, boo, scout, robert, mayella];
+function getInputName(){
+  var employeeName = document.getElementById('nameIn').value;
+
+  console.log(employeeName);
+  window.employeeName = employeeName;
+}
+
+function bonusDetermine(employeeName){
+
+  for(var i = 0; i < employees.length; i++){
+      if(employeeName == employees[ i ][0]){
+
+          alert( employees[i][0] + " found. Fetching Info");
+
+      }
+
+
+
+  }
+
+
+
+}
